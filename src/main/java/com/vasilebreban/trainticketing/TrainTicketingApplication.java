cc113489463e7d44055a6cd85572cfd31aed3de6
Vasi Breban <vbreban09@gmail.com>
2026-05-06 18:06:16 +0300

    init project

---
+++ src/main/java/com/vasilebreban/trainticketing/TrainTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.vasilebreban.trainticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainTicketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainTicketingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vasilebreban/trainticketing/TrainTicketingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasilebreban.trainticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
